94c5c97361bff642f09220eb6bfa61983565de2c
vladrich <vladrich@users.noreply.github.com>
2017-08-16 13:31:22 +0200

    update for the change in github API
    https://github.com/google/go-github/commit/23d6cb9cacb5aa314e93d600fe20a48496a718d4

--- sub/sub.go
+++ sub/sub.go
@@ -2,6 +2,7 @@ package sub
 
 import "strconv"
 import "fmt"
+import "context"
 import "crypto/rsa"
 import "crypto/rand"
 import "github.com/google/go-github/github"
@@ -12,7 +13,7 @@ func Foo() {
 
     // list public repositories for org "github"
     opt := &github.RepositoryListByOrgOptions{Type: "public"}
-    repos, _, _ := client.Repositories.ListByOrg("github", opt)
+    repos, _, _ := client.Repositories.ListByOrg(context.Background(), "github", opt)
 
     fmt.Println("Hello from sub! Num repos: " + strconv.Itoa(len(repos)))
 
